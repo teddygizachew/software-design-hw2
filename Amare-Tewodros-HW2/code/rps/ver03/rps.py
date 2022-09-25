60f5a8f4b07e72ff8fca748d1657b0ec7973a4ee
teddygizachew <teddygizachew1@gmail.com>
2022-09-24 21:19:36 -0400

    version 0.3 - update

--- Amare-Tewodros-HW2/code/rps/ver03/rps.py
+++ Amare-Tewodros-HW2/code/rps/ver03/rps.py
@@ -5,26 +5,29 @@ class Strategy(object):
     def execute(self):
         pass
 
-    def print_header(self):
-        print(f"{emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')} GAME MODE {emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')}")
 
 class BestTwoOfThreeRPS(Strategy):
     def execute(self):
-        print(f"{emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')} GAME MODE {emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')}")
+        print(
+            f"{emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')} GAME MODE {emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')}")
         print(f"{emoji.emojize(':video_game:') * 2} Best 2 out of 3 {emoji.emojize(':video_game:') * 2}")
         print()
 
 
 class NoRepeatMoveRPS(Strategy):
     def execute(self):
-        print(f"Use Bus - Time (1 hours): Cost ($5)")
+        print(
+            f"{emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')} GAME MODE {emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')}")
+        print(f"{emoji.emojize(':video_game:') * 2} No Repeating The Same Move {emoji.emojize(':video_game:') * 2}")
+        print()
 
 
 class NormalRPS(Strategy):
     def execute(self):
-        print(f"{emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')} GAME MODE {emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')}")
-        print(f"{emoji.emojize(':video_game:') * 2} Best 2 out of 3 Mode {emoji.emojize(':video_game:') * 2}")
-        print(f"Use Taxi - Time (0.1 hours): Cost ($20)")
+        print(
+            f"{emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')} GAME MODE {emoji.emojize(':rock:')} {emoji.emojize(':newspaper:')} {emoji.emojize(':scissors:')}")
+        print(f"{emoji.emojize(':video_game:') * 2} Normal {emoji.emojize(':video_game:') * 2}")
+        print()
 
 
 class Context(object):
@@ -47,15 +50,13 @@ class Context(object):
 # Client code    
 def main():
     c = Context()
-    # best 2 out of 3
+
     best_two_of_three = "best2of3"
     no_repeat_move = "no_repeat_move"
     normal = "normal"
 
     c.make_decision(best_two_of_three)
-
     c.make_decision(no_repeat_move)
-
     c.make_decision(normal)
 
 
